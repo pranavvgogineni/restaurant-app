6f3ecc82f54c1ae291ca45b5fa7d8c13dff78c0b
Pranav Gogineni <pranavgv@gmail.com>
2023-09-03 09:50:31 -0500

    Adds user controller and adds create function with a test function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      city: params[:city],
+      zipcode: params[:zipcode],
+    )
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+has_secure_password
+validates :email, presence: true, uniqueness: true
 has_many :saved_searches
 
 class User < ApplicationRecord
