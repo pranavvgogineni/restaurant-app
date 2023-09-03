f5ee7ca3b8346466e12a50f618e5390773852f07
Pranav Gogineni <pranavgv@gmail.com>
2023-09-03 09:50:47 -0500

    Adds user controller and adds create function with a test function

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "create" do
     assert_difference "User.count", 1 do
-      post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+      post "/users.json", params: { first_name: "Test", last_name: "Test", email: "test@test.com", password: "password", password_confirmation: "password", city: "Test City", state: "Test State", zipcode: 1111 }
       assert_response 201
     end
   end
