dc11b77c283d5563320821a36d3263fbbeee65ff
Pranav Gogineni <pranavgv@gmail.com>
2023-09-03 09:52:53 -0500

    adds json messages to provide messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,11 @@ class UsersController < ApplicationController
       city: params[:city],
       zipcode: params[:zipcode],
     )
+
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 end
