0efd42570baa5625bb84e33ad346154a85fdc8bd
Pranav Gogineni <pranavgv@gmail.com>
2023-09-03 12:08:06 -0500

    Adds create action

--- app/controllers/saved_searches_controller.rb
+++ app/controllers/saved_searches_controller.rb
@@ -5,5 +5,19 @@ class SavedSearchesController < ApplicationController
   end
 
   def create
+    @saved_search = SavedSearch.create(
+      user_id: params[:user_id],
+      radius: params[:radius],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      price_range: params[:price_range],
+      cuisine: params[:price_range],
+    )
+    if @saved_search.valid?
+      render :show
+    else
+      render json: { errors: @saved_search.errors.full_messages }, status: 422
+    end
   end
 end
--- test/controllers/saved_searches_controller_test.rb
+++ test/controllers/saved_searches_controller_test.rb
@@ -8,4 +8,12 @@ class SavedSearchesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal SavedSearch.count, data.length
   end
+
+  test "create" do
+    assert_difference "SavedSearch.count", 1 do
+      user = User.create(first_name: "Test", last_name: "Test", email: "test@test.com", password: "password", password_confirmation: "password", city: "Test City", state: "Test State", zipcode: 1111)
+      post "/saved_searches.json", params: { user_id: user.id, radius: 100, city: "Brookfield", state: "Wisconsin", zip: 53045, price_range: 2, cuisine: "Italian" }
+      assert_response 200
+    end
+  end
 end
