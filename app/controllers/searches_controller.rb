9b493b999ef5203382d12b32eadd454dcc2060db
Pranav Gogineni <pranavgv@gmail.com>
2023-10-09 14:45:00 -0500

    test

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
   def index
     results = Geocoder.search(params[:address])
     coordinates = results.first.coordinates
-    response = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=#{params[:cuisine]}&location=#{coordinates[0]}%2C#{coordinates[1]}&radius=#{params[:radius]}&type=restaurant&key=AIzaSyBJ7yHcf0lOrVWIQ1pJdUl5bj-OVjv1A98")
+    response = HTTP.get()
 
     data = JSON.parse(response.body)
     render json: data
